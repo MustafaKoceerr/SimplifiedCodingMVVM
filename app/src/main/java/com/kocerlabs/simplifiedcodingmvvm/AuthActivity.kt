4b3f52331a7dc973e5dce90d2de0b15e5a379d8d
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-11-29 13:44:35 +0300

    id's defined

--- app/src/main/java/com/kocerlabs/simplifiedcodingmvvm/AuthActivity.kt
+++ app/src/main/java/com/kocerlabs/simplifiedcodingmvvm/AuthActivity.kt
@@ -11,7 +11,7 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootAuth)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
