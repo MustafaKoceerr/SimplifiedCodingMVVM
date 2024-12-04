f135dc9f3f40c12eb6f870250e331c66947a99e2
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-04 16:27:04 +0300

    DataStore Preferences defined

---
+++ app/src/main/java/com/kocerlabs/simplifiedcodingmvvm/data/UserPreferences.kt
@@ -0,0 +1,40 @@
+package com.kocerlabs.simplifiedcodingmvvm.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+class UserPreferences(
+    context: Context
+) {
+    private val applicationContext = context.applicationContext
+
+    // create datastore Instance
+
+
+// datastore works with Kotlin coroutine and flow
+    // bu yüzden değeri getirmek için flow'a ihtiyacımız var
+
+    val authToken: Flow<String>
+        get() = applicationContext.dataStore.data.map { preferences ->
+            preferences[KEY_AUTH] ?: ""
+        }
+
+    suspend fun saveAuthToken(authToken: String) {
+        applicationContext.dataStore.edit { preferences ->
+            preferences[KEY_AUTH] = authToken
+        }
+    }
+
+    companion object {
+        // At the top level of your kotlin file:
+        val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "my_data_store")
+        private val KEY_AUTH = stringPreferencesKey("my_data_store")
+    }
+}
