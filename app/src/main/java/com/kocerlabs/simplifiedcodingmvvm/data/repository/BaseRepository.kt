1f0dbdef0af1f021ce71e2921765c3290f3a5575
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-04 16:26:29 +0300

    Base structures defined

---
+++ app/src/main/java/com/kocerlabs/simplifiedcodingmvvm/data/repository/BaseRepository.kt
@@ -0,0 +1,29 @@
+package com.kocerlabs.simplifiedcodingmvvm.data.repository
+
+import com.kocerlabs.simplifiedcodingmvvm.data.network.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+abstract class BaseRepository (){
+
+
+    suspend fun <T> safeApiCall(
+        apiCall: suspend () -> T
+    ): Resource<T> {
+        return withContext(Dispatchers.IO) { //api call'unu IO dispatcher'da gerçekleştirmek istediğim için
+            try {
+                Resource.Success(apiCall.invoke()) // invoke ile api call'u çalıştırıyoruz.
+            }catch (throwable: Throwable){
+                when(throwable){
+                    is HttpException -> {
+                        Resource.Failure<Nothing>(false, throwable.code(), throwable.response()?.errorBody())
+                    }
+                    else->{
+                        Resource.Failure<Nothing>(true, null, null)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/kocerlabs/simplifiedcodingmvvm/ui/base/BaseFragment.kt
@@ -0,0 +1,50 @@
+package com.kocerlabs.simplifiedcodingmvvm.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.ViewBinding
+import com.kocerlabs.simplifiedcodingmvvm.data.UserPreferences
+import com.kocerlabs.simplifiedcodingmvvm.data.network.model.RemoteDataSource
+import com.kocerlabs.simplifiedcodingmvvm.data.repository.BaseRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+// Data binding, view model'e ihtiyacımız var.
+abstract class BaseFragment<VM : ViewModel, B : ViewBinding, R : BaseRepository> : Fragment() {
+// Buradaki abstract fonksiyonları kullanarak, gerçek fragment'a geçince ihtiyacımız olan class'ları verecekler.
+
+    protected lateinit var userPreferences: UserPreferences
+
+    protected lateinit var binding: B
+    protected val remoteDataSource = RemoteDataSource()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = getFragmentBinding(inflater, container)
+        userPreferences = UserPreferences(requireContext())
+
+        lifecycleScope.launch {
+            userPreferences.authToken.first()
+        }
+
+        return binding.root
+    }
+
+    abstract fun getViewModel(): Class<VM>
+
+    abstract fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    ): B
+
+    abstract fun getFragmentRepository(): R
+
+}
